53635b8f564eb10e849abda96314395364c93054
Kanak05 <kanak.nagar64@gmail.com>
2025-07-25 23:59:06 +0530

    host changed

--- ca_website/settings.py
+++ ca_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-58(%gw)-e32+u04u+)lqkcnb+xw_3bja4$ix(9iudy&73+r-$b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
