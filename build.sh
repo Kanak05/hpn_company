0ed706ebd9740b15acf552e27d2b0660a48724ad
Kanak05 <kanak.nagar64@gmail.com>
2025-07-25 20:13:12 +0530

    Add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Making migrations..."
+python manage.py makemigrations --noinput
+echo "Applying migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+echo "Build process completed!"
