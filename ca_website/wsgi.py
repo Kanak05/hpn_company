1964f29c18815a021a690307116c9467731a346d
Kanak05 <kanak.nagar64@gmail.com>
2025-07-26 02:04:29 +0530

    last

--- ca_website/wsgi.py
+++ ca_website/wsgi.py
@@ -1,10 +1,9 @@
-from whitenoise import WhiteNoise
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ca_website.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'staticfiles'))
-
-app = application  # ✅ Required by Vercel
+app = application  # Vercel requires 'app'
